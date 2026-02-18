93630e752df87c20dadc2751eca9bac02b172f32
kizzor <105450529+kizzor@users.noreply.github.com>
2026-02-18 09:38:30 +0000

    Fix Next.js config to use JS syntax

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // add any other config here
+  // Add any other config options here
 };
 
 module.exports = nextConfig;
